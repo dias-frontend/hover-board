98e473901665cec1cfc781066e346aed11196c77
Omirbekov Dias <dias_zaysan@mail.ru>
2026-04-03 15:56:15 +0500

    hover board

---
+++ app.js
@@ -0,0 +1,28 @@
+const board = document.querySelector("#board");
+const SQUARES_NUMBER = 500;
+const colors = ["red", "blue", "green", "yellow", "purple"];
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
